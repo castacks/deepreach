0d10ed13956807ecefcb8e5eddf448cf302df653
Jay Patrikar <jaypat@v008.ib.bridges2.psc.edu>
2023-11-07 15:29:55 -0500

    acc dynamics

--- loss_functions.py
+++ loss_functions.py
@@ -129,3 +129,72 @@ def initialize_hji_air3D(dataset, minWith):
                 'diff_constraint_hom': torch.abs(diff_constraint_hom).sum()}
 
     return hji_air3D
+
+def initialize_hji_air5D(dataset, minWith):
+    # Initialize the loss function for the air3D problem
+    # The dynamics parameters
+    omega_max = dataset.omega_max
+    acc_max = dataset.acc_max
+    alpha = dataset.alpha
+
+    def hji_air5D(model_output, gt):
+        source_boundary_values = gt['source_boundary_values']
+        x = model_output['model_in']  # (meta_batch_size, num_points, 4)
+        y = model_output['model_out']  # (meta_batch_size, num_points, 1)
+        dirichlet_mask = gt['dirichlet_mask']
+        batch_size = x.shape[1]
+
+        du, status = diff_operators.jacobian(y, x)
+        dudt = du[..., 0, 0]
+        dudx = du[..., 0, 1:]
+
+        x_u = x * 1.0
+        x_u[..., 1] = x_u[..., 1] * alpha['x'] 
+        x_u[..., 2] = x_u[..., 2] * alpha['y'] 
+        x_u[..., 3] = x_u[..., 3] * alpha['th'] 
+        x_u[..., 4] = x_u[..., 4] * alpha['v'] + alpha['v'] 
+        x_u[..., 5] = x_u[..., 5] * alpha['v'] + alpha['v']
+
+        # x_theta = x[..., 3] * 1.0
+
+        # Scale the costate for theta appropriately to align with the range of [-pi, pi]
+        dudx[..., 0] = dudx[..., 0] / alpha['x']
+        dudx[..., 1] = dudx[..., 1] / alpha['y']
+        dudx[..., 2] = dudx[..., 2] / alpha['th']
+        dudx[..., 3] = dudx[..., 3] / alpha['v']
+        dudx[..., 4] = dudx[..., 4] / alpha['v']
+        # Scale the coordinates
+        # x_theta = alpha['th'] * x_theta
+
+        # Air5D dynamics
+        # \dot x    = -v_a + v_b \cos \psi + a y
+        # \dot y    = v_b \sin \psi - a x
+        # \dot \psi = b - a
+        # \dot \v_a = a_a
+        # \dot \v_b = a_b 
+
+        # Compute the hamiltonian for the ego vehicle
+        ham = omega_max * torch.abs(dudx[..., 0] * x_u[..., 2] - dudx[..., 1] * x_u[..., 1] - dudx[..., 2])  + acc_max * torch.abs(dudx[..., 3])# Control component
+        ham = ham - omega_max * torch.abs(dudx[..., 2]) - acc_max * torch.abs(dudx[...,4]) # Disturbance component
+        ham = ham + dudx[..., 0]*(-x_u[..., 4] + x_u[..., 5]*torch.cos(x_u[...,3])) + dudx[..., 1]*(x_u[..., 5] * torch.sin(x_u[...,3]))
+        # Effect of time factor
+        ham = ham * alpha['time']
+        # If we are computing BRT then take min with zero
+        if minWith == 'zero':
+            ham = torch.clamp(ham, max=0.0)
+
+        if torch.all(dirichlet_mask):
+            diff_constraint_hom = torch.Tensor([0])
+        else:
+            diff_constraint_hom = dudt - ham
+            if minWith == 'target':
+                diff_constraint_hom = torch.max(diff_constraint_hom[:, :, None], y - source_boundary_values)
+
+        dirichlet = y[dirichlet_mask] - source_boundary_values[dirichlet_mask]
+
+        # A factor of 15e2 to make loss roughly equal
+        return {'dirichlet': torch.abs(dirichlet).sum() * batch_size / 15e2,
+                'diff_constraint_hom': torch.abs(diff_constraint_hom).sum()}
+
+    return hji_air5D
+
